334aa6bdfbfad1bc7aaad237f4b555fe6b8ca024
Pibloso <duncan.mccorkell@protonmail.com>
2025-02-27 23:22:31 -0700

    Report output formatting

--- main.py
+++ main.py
@@ -9,7 +9,14 @@ def get_book_text(filepath):
 def main():
     text = get_book_text("./books/frankenstein.txt")
     count = word_count(text)
-    print(f"{count} words found in the document")
-    print(letter_count(text))
-
+    print(f"""============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {count} total words
+--------- Character Count -------""")
+    pairs = sorted(letter_count(text).items())
+    for pair in pairs:
+        if pair[0].isalpha() == True:
+            print(f"{pair[0]}: {pair[1]}")
+    print("============= END ===============")
 main()
