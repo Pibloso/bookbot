22dc36c4f5ccfe193c921408511ebed51ee8017c
Pibloso <duncan.mccorkell@protonmail.com>
2025-03-02 22:33:30 -0700

    Final: Add Book Argument

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+import sys
 from stats import word_count, letter_count
+
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 def get_book_text(filepath):
     with open(filepath) as f:
         contents = f.read()
     return contents
 
-
 def main():
-    text = get_book_text("./books/frankenstein.txt")
+    text = get_book_text(sys.argv[1])
     count = word_count(text)
     print(f"""============ BOOKBOT ============
-Analyzing book found at books/frankenstein.txt...
+Analyzing book found at {sys.argv[1]}...
 ----------- Word Count ----------
 Found {count} total words
 --------- Character Count -------""")
